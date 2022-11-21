a9fb9e77056e3cd788bf3d8fcabd0b65883f94d1
rrantxa <arantxa.meza@gmail.com>
2022-11-20 23:16:44 -0600

    Adding first and second challenge deliverables.

---
+++ SQL_Queries/crowdfunding_db_schema_2.sql
@@ -0,0 +1,48 @@
+CREATE TABLE campaign(
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
+	goal NUMERIC(10,2) NOT NULL,
+	pledged NUMERIC(10,2) NOT NULL,
+	outcome VARCHAR(50) NOT NULL,
+	backers_count INT,
+	country VARCHAR(10) NOT NULL,
+	currency VARCHAR(10) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(10) NOT NULL,
+	subcategory_id VARCHAR(10) NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
+);	
+
+SELECT * FROM campaign;
+
+CREATE TABLE category (
+	category_id VARCHAR(10) NOT NULL,
+	category VARCHAR(50) NOT NULL,
+	PRIMARY KEY(category_id)
+);	
+
+SELECT * FROM category;
+
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(10) NOT NULL,
+	subcategory VARCHAR(50) NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);	
+
+SELECT * FROM subcategory;
+
+CREATE TABLE contacts (
+	contact_id INT NOT NULL, 
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100),
+	PRIMARY KEY (contact_id)
+);	
+
+SELECT * FROM contacts;
