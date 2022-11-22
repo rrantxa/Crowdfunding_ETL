4615d1c158e6497db681b3ea9d461a99d5a4dfa7
rrantxa <arantxa.meza@gmail.com>
2022-11-21 18:18:47 -0600

    Correcting mistakes on third deliverable.

--- Queries/crowdfunding_db_schema.sql
+++ Queries/crowdfunding_db_schema.sql
@@ -1,27 +1,4 @@
 -- NOTE : The ALTER TABLE constraint didn't seem necessary to add to the tables, as they already had their primary and foreign keys from the moment they were created. 
-CREATE TABLE campaign(
-	cf_id INT NOT NULL,
-	contact_id INT NOT NULL,
-	company_name VARCHAR(100) NOT NULL,
-	description TEXT NOT NULL,
-	goal NUMERIC(10,2) NOT NULL,
-	pledged NUMERIC(10,2) NOT NULL,
-	outcome VARCHAR(50) NOT NULL,
-	backers_count INT,
-	country VARCHAR(10) NOT NULL,
-	currency VARCHAR(10) NOT NULL,
-	launch_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	category_id VARCHAR(10) NOT NULL,
-	subcategory_id VARCHAR(10) NOT NULL,
-	PRIMARY KEY (cf_id),
-	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
-	FOREIGN KEY (category_id) REFERENCES category (category_id),
-	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
-);	
-
-SELECT * FROM campaign;
-
 CREATE TABLE category (
 	category_id VARCHAR(10) NOT NULL,
 	category VARCHAR(50) NOT NULL,
@@ -47,6 +24,29 @@ CREATE TABLE contacts (
 );	
 
 SELECT * FROM contacts;
+
+CREATE TABLE campaign(
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
+	goal NUMERIC(10,2) NOT NULL,
+	pledged NUMERIC(10,2) NOT NULL,
+	outcome VARCHAR(50) NOT NULL,
+	backers_count INT,
+	country VARCHAR(10) NOT NULL,
+	currency VARCHAR(10) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(10) NOT NULL,
+	subcategory_id VARCHAR(10) NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
+);	
+
+SELECT * FROM campaign;
 
 CREATE TABLE backers (
 	backer_id VARCHAR(10) NOT NULL,
